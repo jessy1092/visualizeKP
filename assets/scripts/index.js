72e8381f00be6ce297db2fd1918ab5145b5efbfd
Lee <jessy1092@gmail.com>
2014-09-06 11:00:17 +0800

    init video view count.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -34,7 +34,7 @@ $(document).ready(function ()
                     cheklength++;
                     videosList = videosList.concat(videos.data);
                     if (cheklength == playList.data.length) {
-                        console.log(videosList);
+                        // console.log(videosList);
                         updateVideosCount();
                     }
                 });
@@ -65,25 +65,27 @@ $(document).ready(function ()
     }
 
     function updateVideosCount () {
-        var cheklength = 0;
+        var checklength = 0;
         videosList.forEach(function (videosEntry) {
+            // console.log(videosEntry.title);
+            videosEntry.viewCount = 0;
             $.ajax({
                 type: 'GET',
                 url: youtubeApiServer + videosEntry.id + '?alt=json',
                 dataType: 'json'
             }).done(function (videosCountData) {
-                cheklength++;
+                checklength++;
                 // console.log(videosCountData);
                 videosEntry.viewCount = parseInt(videosCountData.entry.yt$statistics.viewCount);
                 videosEntry.rating = videosCountData.entry.gd$rating.average;
                 videosTotalViewsCount += videosEntry.viewCount;
-                // console.log(videosTotalViewsCount);
-                if (cheklength == videosList.length)
-                {
+                // console.log(videosEntry.title);
+                // if (checklength == videosList.length)
+                // {
                     $('.totalViewsCnt').text(videosTotalViewsCount);
                     d3ViewVideosCountVisual();
-                }
-            })
+                // }
+            });
         })
     }
 
@@ -95,7 +97,7 @@ $(document).ready(function ()
             children: videosList.map(function (entry) {
                 max = max > entry.viewCount ? max : entry.viewCount;
                 min = min < entry.viewCount ? min : entry.viewCount;
-                console.log(entry.viewCount);
+                // console.log('view:' + entry.viewCount + 'test:' + entry.title);
                 return {
                     value: entry.viewCount,
                     videosEntry: entry
@@ -107,7 +109,7 @@ $(document).ready(function ()
                         .size([680, 680]).padding(20).nodes(dataNode);
 
         d3Pack.shift();
-        console.log(d3Pack);
+        // console.log(d3Pack);
         var colorScale = d3.scale.linear().domain([min, max]).range(['#0aa', '#0a5']);
 
         var dataPack = d3.select('.videosCount').selectAll('circle.pack').data(d3Pack);
@@ -120,6 +122,22 @@ $(document).ready(function ()
             r: function (d) { return d.r; },
             fill: function (d) { return colorScale(d.value); },
             stroke: '#fff'
+        });
+
+        console.log(d3Pack);
+        var dataText = d3.select('.videosCount').selectAll('text.pack').data(d3Pack);
+        dataText.enter().append('text').attr('class', 'pack').style('pointer-events', 'none');
+
+        d3.select('.videosCount').selectAll('text.pack').attr({
+            x: function (d) { return d.x; },
+            y: function (d) { return d.y; },
+            fill: '#fff',
+            'text-anchor': 'middle',
+            'dominant-baseline': 'central'
+        }).text(function (d) {
+            console.log(d.videosEntry.title);
+            var ans = d.value > 20000 ? d.videosEntry.title.substring(0, d.r/4.5) :'';
+            return ans;
         });
     }
 });
